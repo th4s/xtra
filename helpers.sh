6d20036ae1c7f83cc65ed2aefd1df4e5dec4c1ff
th4s <th4s@metavoid.xyz>
2021-05-11 13:02:15 +0200

    added helper functions

---
+++ helpers.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Mount chaindata: sshfs giga:/var/lib/geth/geth/chaindata/ancient /home/thojest/chaindata_ancient/ -o reconnect,ro
+
+rif() {
+    tmpvar=$(od -v -A d -t x1 -j $((6 * $2)) -N 6 --endian big $1 | head -n1 |cut -d" " -f2-3 | tr -d " ")
+    printf "%d\n" 0x${tmpvar}
+}
+
+rio() {
+    tmpvar=$(od -v -A d -t x1 -j $((6 * $2)) -N 6 --endian big $1 | head -n1 |cut -d" " -f4-7 | tr -d " ")
+    printf "%d\n" 0x${tmpvar}
+}
+
+refor() {
+    tmpfilevar=$(rif $1 $2)
+    tmpoffsetvar=$(rio $1 $2)
+    tmpoffsetvar2=$(rio $1 $(($2 + 1)))
+    echo "File: ${tmpfilevar} Start: ${tmpoffsetvar} Length: $((${tmpoffsetvar2} - ${tmpoffsetvar} - 1))"
+}
+
+rb() {
+    od -v -A d -t x1 --endian big -j $2 -N $3 $1
+}
+
+rbh() {
+    od -v -A d -t c --endian big -j $2 -N $3 $1
+}
+
+wb() {
+    dd if=$1 bs=1 skip=$2 count=$3
+}
