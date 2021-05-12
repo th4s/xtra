b6cd2603f4d6a58adf4fa38faea4de47c445af12
th4s <th4s@metavoid.xyz>
2021-05-12 19:40:08 +0200

    Adapted benchmark to use headers instead of bodies

--- benches/freezer_load.rs
+++ benches/freezer_load.rs
@@ -2,26 +2,26 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use std::path::PathBuf;
 use xtra::freezer::BlockPart;
 
-const BLOCKS: u64 = 100_000;
+const HEADERS: u64 = 999;
 
-pub fn c_load_bodies(c: &mut Criterion) {
+pub fn c_load_headers(c: &mut Criterion) {
     dotenv::from_filename("bench.env").expect("Environment file bench.env not found");
     let ancient_folder = PathBuf::from(
-        std::env::var("FREEZER_LOAD_BODIES")
-            .expect("Environment variable FREEZER_LOAD_BODIES not found"),
+        std::env::var("FREEZER_LOAD_HEADERS")
+            .expect("Environment variable FREEZER_LOAD_HEADERS not found"),
     );
-    c.bench_function(&format!("freezer_load_bodies_{}", BLOCKS), |bencher| {
+    c.bench_function(&format!("freezer_load_headers{}", HEADERS), |bencher| {
         bencher.iter(|| {
-            BlockPart::Bodies
-                .load(ancient_folder.as_path(), 0, BLOCKS)
+            BlockPart::Headers
+                .load(ancient_folder.as_path(), 0, HEADERS)
                 .unwrap()
         })
     });
 }
 
 criterion_group! {
-name = load_bodies;
-config = Criterion::default();
-targets = c_load_bodies
+name = load_headers;
+config = Criterion::default().sample_size(50);
+targets = c_load_headers
 }
-criterion_main!(load_bodies);
+criterion_main!(load_headers);
